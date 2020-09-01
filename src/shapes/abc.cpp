c5353804733c57fba27f5452e0e8cdc9c3671223
SergeyShlyaev <63272131+SergeyShlyaev@users.noreply.github.com>
2020-08-31 22:16:32 -0400

    support for loading multiple meshes
    Added support for loading multiple meshes from file
    Added support for `shape_name` to specify object by name

--- src/shapes/abc.cpp
+++ src/shapes/abc.cpp
@@ -5,7 +5,6 @@
 #include <mitsuba/core/properties.h>
 #include <mitsuba/core/util.h>
 #include <mitsuba/core/timer.h>
-#include <enoki/half.h>
 #include <unordered_map>
 #include <fstream>
 
@@ -28,11 +27,16 @@ ABC (Alembic) mesh loader (:monosp:`abc`)
 
  * - filename
    - |string|
-   - Filename of the ABC file that should be loaded 
+   - Filename of the Alembic file to load 
+ * - shape_name
+   - |string|
+   - Alembic file may contain several separate meshes. This optional parameter
+     specifies name of mesh to load. (Default: not specified, i.e. load all objects)
  * - shape_index
    - |int|
-   - A :monosp:`.serialized` file may contain several separate meshes. This parameter
-     specifies which one should be loaded. (Default: 0, i.e. the first one)
+   - Alembic file may contain several separate meshes. This optional parameter
+     specifies which mesh should be loaded. (Default: 0, i.e. the first one) If both 
+     shape_name and shape_index are specified, shape_name takes precedence.
  * - m_flip_normals
    - |bool|
    - Will flip normals on the mesh. Some programs output face indexes in clockwise order, some in counter-clockwise. 
@@ -70,22 +74,23 @@ using namespace Alembic::AbcCoreAbstract;
 template <typename Float, typename Spectrum>
 class AlembicMesh final : public Mesh<Float, Spectrum> {
 public:
-    MTS_IMPORT_BASE(Mesh, m_name, m_bbox, m_to_world, m_vertex_count, m_face_count,
+    MTS_IMPORT_BASE(Mesh, m_bsdf, m_name, m_bbox, m_to_world, m_vertex_count, m_face_count,
                     m_vertex_positions_buf, m_vertex_normals_buf, m_vertex_texcoords_buf,
-                    m_faces_buf, add_attribute, m_disable_vertex_normals, has_vertex_normals,
+                    m_faces_buf, m_disable_vertex_normals, has_vertex_normals,
                     has_vertex_texcoords, recompute_vertex_normals, set_children)
-    MTS_IMPORT_TYPES()
+    MTS_IMPORT_TYPES(BSDF, Shape)
 
-    using typename Base::MeshAttributeType;
+    // Mesh is always stored in single precision
+    using InputFloat = float;
+    using InputPoint3f  = Point<InputFloat, 3>;
+    using InputVector2f = Vector<InputFloat, 2>;
+    using InputVector3f = Vector<InputFloat, 3>;
+    using InputNormal3f = Normal<InputFloat, 3>;
+    using FloatStorage = DynamicBuffer<replace_scalar_t<Float, InputFloat>>;
+
     using typename Base::ScalarSize;
     using typename Base::ScalarIndex;
-    using typename Base::InputFloat;
-    using typename Base::InputPoint3f;
-    using typename Base::InputVector2f;
-    using typename Base::InputVector3f;
-    using typename Base::InputNormal3f;
-    using typename Base::FloatStorage;
-
+    using typename Base::MeshAttributeType;
     using ScalarIndex3 = std::array<ScalarIndex, 3>;
 
     struct AlembicPolymesh
@@ -103,7 +108,6 @@ public:
 
     bool m_flip_normals = true;
     bool m_load_visible = false;
-    std::string m_mesh_name;
 
     // Alembics can store animation data
     // For now use values at 0 time until Mitsuba supports animaiton
@@ -120,14 +124,24 @@ public:
         // Optionally fix handedness to avoid black renders.
         m_flip_normals = props.bool_("flip_normals", true);
         m_load_visible = props.bool_("load_visible", false);
-        const uint shape_index = props.int_("shape_index", 0);
+        const long shape_index = props.int_("shape_index", -1);
         const bool flip_tex_coords = props.bool_("flip_tex_coords", true);
+        m_shape_name = props.string("shape_name", "");
+        m_use_shape_name = false;
+        bool use_shape_index = false;
+
+        if (!m_shape_name.empty())
+            m_use_shape_name = true;
+
+        // Use shape_index if it is provided and shape_name is not used
+        if (shape_index != -1 && !m_use_shape_name)
+            use_shape_index = true;
 
         auto fail = [&](const std::string &descr) {
-            Throw("Error while loading Alembic file \"%s\": %s!", m_name, descr);
+            Throw("Error while loading Alembic file \"%s\": %s.", m_name, descr);
         };
         auto detailed_fail = [&](const std::string &descr, std::string m_mesh_name="") {
-            Throw("Error while loading Alembic file \"%s\" at object \"%s\". %s!", m_name, m_mesh_name, descr);
+            Throw("Error while loading Alembic file \"%s\" at object \"%s\". %s.", m_name, m_mesh_name, descr);
         };
 
         Log(Debug, "Loading mesh from \"%s\" ..", m_name);
@@ -136,31 +150,41 @@ public:
 
         IFactory factory;
         IFactory::CoreType core_type;
-		IArchive archive = factory.getArchive(file_path.string(), core_type);
+        IArchive archive = factory.getArchive(file_path.string(), core_type);
         IObject archive_top = archive.getTop();
 
         index_t sample_index = 0;
-		std::vector<AlembicPolymesh> alembic_polymeshes;
+        std::vector<AlembicPolymesh> alembic_polymeshes;
         ScalarTransform4f alembic_transform;
 
-        // how do we create multiple mesh shapes?
-        // for now create just one shape specified by shape_index
-        if (shape_index > (uint) archive_top.getNumChildren() - 1)
+        // create one mesh specified by shape_index
+        if (use_shape_index && (shape_index > (uint) archive_top.getNumChildren() - 1) )
             fail(tfm::format("Unable to load mesh. Shape index \"%i\" is "
                                 "out of range 0..%i",
                                 shape_index, archive_top.getNumChildren() - 1));
 
-        IObject object = archive_top.getChild(shape_index);
-        ScalarTransform4f obj_xform = read_abc_xform(object);
-		find_children_recursively(object, sample_index, alembic_transform, alembic_polymeshes);
+        if (use_shape_index)
+            archive_top = archive_top.getChild(shape_index);
+         
+        ScalarTransform4f obj_xform = read_abc_xform(archive_top);
+        find_children_recursively(archive_top, sample_index, alembic_transform, alembic_polymeshes);
 
-        if (alembic_polymeshes.empty())
-            fail("found no polygonal meshes in file.");
+        if (alembic_polymeshes.empty()){
+            std::string error_msg = "found no polygonal meshes in file";
+            if (m_use_shape_name)
+                error_msg += tfm::format(". Got shape_name \"%s\" "
+                                         "please check if this shape was exported into Alembic file" , m_shape_name);
+            if (use_shape_index)
+                error_msg += tfm::format(". Got shape_index \"%i\" "
+                                         "could not get polymesh. Object at \"%i\" is \"%s\"" , 
+                                         shape_index, shape_index, archive_top.getFullName());
+            fail(error_msg);
+        }
 
-        for (size_t each_alembic = 0; each_alembic < alembic_polymeshes.size(); ++each_alembic)
-		{
+        for (size_t each_alembic = 0; each_alembic < alembic_polymeshes.size(); each_alembic++){
             m_mesh_name = alembic_polymeshes[each_alembic].id;
-			IPolyMeshSchema poly_schema = alembic_polymeshes[each_alembic].schema;
+
+            IPolyMeshSchema poly_schema = alembic_polymeshes[each_alembic].schema;
             IPolyMeshSchema::Sample sample = poly_schema.getValue(sample_index);
             IN3fGeomParam normals_param = poly_schema.getNormalsParam();   
             IV2fGeomParam uvs_param = poly_schema.getUVsParam();         
@@ -197,7 +221,7 @@ public:
             ScalarIndex vertex_ctr = 0;
 
             if (polymesh_has_uvs)
-			{
+            {
                 make_attr<IV2fGeomParam, V2fArraySamplePtr, InputVector2f>(
                     uvs_param, num_polygons, vertices_per_face, texcoords);
                 if (flip_tex_coords){
@@ -210,8 +234,8 @@ public:
                 }
             }
 
-			if (!m_disable_vertex_normals && polymesh_has_vertex_normals)
-			{
+            if (!m_disable_vertex_normals && polymesh_has_vertex_normals)
+            {
                 make_attr<IN3fGeomParam, N3fArraySamplePtr, InputNormal3f>(
                     normals_param, num_polygons, vertices_per_face, normals);
                 
@@ -220,11 +244,12 @@ public:
                     InputNormal3f n = normals[i];
                     n = normalize(m_to_world.transform_affine(n));
                     if ( unlikely(!all(enoki::isfinite(n))) ){
-                        Log(Warn, "Invalid vertex normal: %s at vertex %s." , n, i );
+                        Log(Warn,
+                            "Invalid vertex normal: %s at vertex %s of %s.", n, i, m_mesh_name);
                     }
                     normals[i] = n;
                 }
-			}
+            }
 
             size_t uv_ctr=0;
             for (size_t each_face=0; each_face<num_polygons; each_face++){
@@ -291,8 +316,9 @@ public:
 
             m_vertex_count = vertex_ctr;
             m_face_count = (ScalarSize)indices.size();
-            m_faces_buf = DynamicBuffer<UInt32>::copy(indices.data(), m_face_count * 3);
             m_vertex_positions_buf = empty<FloatStorage>(m_vertex_count * 3);
+            m_faces_buf = DynamicBuffer<UInt32>::copy(indices.data(), m_face_count * 3);
+
 
             if (!m_disable_vertex_normals){
                 m_vertex_normals_buf = empty<FloatStorage>(m_vertex_count * 3);   
@@ -302,11 +328,6 @@ public:
                 m_vertex_texcoords_buf = empty<FloatStorage>(m_vertex_count*2);
             }
             
-            m_faces_buf.managed();
-            m_vertex_positions_buf.managed();
-            m_vertex_normals_buf.managed();
-            m_vertex_texcoords_buf.managed();
-
             for (const auto& v_ : vertex_map) {
                 const VertexBinding *v = &v_;
 
@@ -327,24 +348,45 @@ public:
                     v = v->next;
                 }
             }
+            
+            // Mesh constructor in mesh.cpp already has managed(), cuda_sync()
+            // and set_children() calls, so we don't need to call it here
+            m_mesh = new Mesh<Float, Spectrum>(m_mesh_name, m_vertex_count, m_face_count, 
+                                    props, polymesh_has_vertex_normals, polymesh_has_uvs);
 
-            if (arbitrary_properties.valid())
-                add_arbitrary_geom_params(arbitrary_properties);
+            m_mesh->vertex_positions_buffer() = m_vertex_positions_buf;
+            m_mesh->faces_buffer() = m_faces_buf;
+            m_mesh->vertex_normals_buffer() = m_vertex_normals_buf;
+            m_mesh->vertex_texcoords_buffer() = m_vertex_texcoords_buf;
 
+            m_mesh->recompute_bbox();
             if (!m_disable_vertex_normals && normals.empty()) {
                 Timer timer2;
-                recompute_vertex_normals();
+                m_mesh->recompute_vertex_normals();
                 Log(Warn, "\"%s\": computed vertex normals for %s (took %s)", m_name, m_mesh_name,
                     util::time_string(timer2.value()));
             }
-        }
-        if constexpr (is_cuda_array_v<Float>)
-            cuda_sync();
 
-        set_children();    
+            if (arbitrary_properties.valid())
+                add_arbitrary_geom_params(arbitrary_properties);    
+
+            m_mesh_objects.push_back(m_mesh);
+        }
     }
 
+    std::vector<ref<Object>> expand() const override {       
+        return m_mesh_objects;
+    }
+
+protected:
+    std::string m_mesh_name;
+
 private:
+    ref<Mesh<Float, Spectrum>>  m_mesh;
+    std::vector<ref<Object>> m_mesh_objects;
+    std::string m_shape_name;
+    bool m_use_shape_name;
+
     std::unordered_map<std::string, std::string> known_attr = {
         {"Cd","color"},
         {"color","color"},
@@ -453,11 +495,11 @@ private:
     {
         std::string attr_name = property_header.getName();
         geom_param_t param(parent, property_header.getName());
-        ISampleSelector sample_selector( m_alembic_time ); // alembic_time, for now just use value at 0 time
+        ISampleSelector sample_selector( m_alembic_time );
         typename geom_param_t::sample_type param_sample;
         param.getExpanded(param_sample, sample_selector);
 
-        // 3 for color; 1 for shop_materialpath
+        // size, e.g. 3 for color; 1 for shop_materialpath
         size_t extent = geom_param_t::prop_type::traits_type::dataType().getExtent();
         const pod_t *values = reinterpret_cast<const pod_t *>(
                 param_sample.getVals()->get());
@@ -469,7 +511,8 @@ private:
                 //Point attribute
                 if ( !(known_attr.find(attr_name) == known_attr.end()) )
                     attr_name = "vertex_"+known_attr[attr_name];
-                add_attribute(attr_name, extent, FloatStorage::copy(values, m_vertex_count * extent));
+                m_mesh->add_attribute(attr_name, extent, 
+                                    FloatStorage::copy(values, m_vertex_count * extent));
                 break;
             case kFacevaryingScope:{
                 // Vertex attribute
@@ -478,7 +521,8 @@ private:
 
                 bool is_vertex_attr = attr_name.find("vertex_") == 0;
                 if(is_vertex_attr){
-                    add_attribute(attr_name, extent, FloatStorage::copy(values, m_vertex_count * extent));
+                    m_mesh->add_attribute(attr_name, extent, 
+                                    FloatStorage::copy(values, m_vertex_count * extent));
                 }
                 else{
                     Log(Info, "Skipping vertex attribute \"%s\" on \"%s\" as it does not start with \"vertex_\"", attr_name, m_mesh_name);
@@ -494,7 +538,8 @@ private:
 
                 bool is_face_attr = attr_name.find("face_") == 0;
                 if(is_face_attr){
-                    add_attribute(attr_name, extent, FloatStorage::copy(values, m_face_count * extent));
+                    m_mesh->add_attribute(attr_name, extent, 
+                                    FloatStorage::copy(values, m_face_count * extent));
                 }
                 else{
                     Log(Info, "Skipping face attribute \"%s\" on \"%s\" as it does not start with \"face_\"", attr_name, m_mesh_name);
@@ -521,7 +566,6 @@ private:
             std::vector<mitsuba_t> data_per_face;
 
             for (size_t vertex_index=0; vertex_index<vertex_count_in_face; vertex_index++, data_index++){
-                //Vertex normal
                 mitsuba_t n;
                 for (size_t j = 0; j < extent; j++){
                     n[j] = (*param_sample_values)[data_index][j];
@@ -538,16 +582,16 @@ private:
     }
 
     void find_children_recursively(const IObject& obj,
-		index_t sample_index,
-		ScalarTransform4f &t, std::vector<AlembicPolymesh>& map)
-	{
-		for (size_t i = 0; i < obj.getNumChildren(); i++)
-		{
-			ScalarTransform4f child_transform = t;
-			IObject child = obj.getChild(i);
+        index_t sample_index,
+        ScalarTransform4f &t, std::vector<AlembicPolymesh>& polymeshes)
+    {
+        for (size_t i = 0; i < obj.getNumChildren(); i++)
+        {
+            ScalarTransform4f child_transform = t;
+            IObject child = obj.getChild(i);
 
-			if ( IXform::matches(child.getMetaData()) ){
-				IXform xform_obj = IXform(child, kWrapExisting);
+            if ( IXform::matches(child.getMetaData()) ){
+                IXform xform_obj = IXform(child, kWrapExisting);
                 IXformSchema &xs = xform_obj.getSchema();
                 XformSample xform_sample = xs.getValue(ISampleSelector(m_alembic_time));
 
@@ -559,24 +603,32 @@ private:
                     child_transform = obj_xform;
                 }
             }
-			else if (IPolyMesh::matches(child.getMetaData())){
-				IPolyMesh poly_obj(child, kWrapExisting);
-				AlembicPolymesh polymesh;
-				polymesh.schema = poly_obj.getSchema();
-				polymesh.id = child.getFullName();
-				polymesh.xform = t;
-                if (!m_load_visible || GetVisibilityProperty(child) == (bool)kVisibilityVisible){
-                    map.push_back(polymesh);
+            else if (IPolyMesh::matches(child.getMetaData())){
+                bool mesh_visible = !m_load_visible || GetVisibilityProperty(child) == (bool)kVisibilityVisible;
+                bool add_mesh = true;
+                IPolyMesh poly_obj(child, kWrapExisting);
+                AlembicPolymesh polymesh;
+                polymesh.schema = poly_obj.getSchema();
+                polymesh.id = child.getFullName();
+                polymesh.xform = t;
+
+                if (m_use_shape_name){
+                    // check if shape name is in alembic's mesh name to avoid
+                    // typing full names. E.g. "sphere" instead of /sphere_object1/sphere1
+                    // if shape name from Xml file is not in mesh name, skip mesh
+                    if (child.getFullName().find(m_shape_name) == std::string::npos)
+                        add_mesh =false;
                 }
-                else if (GetVisibilityProperty(child) == (bool)kVisibilityHidden){
-                    // Issue a message in the log for invisible object perhaps
-                }                       
-				continue;
-			}
+
+                if (mesh_visible && add_mesh)
+                    polymeshes.push_back(polymesh);
+
+                continue;
+            }
             // continue traversing alembic tree for more child objects
-			find_children_recursively(child, sample_index, child_transform, map);
-		}
-	}
+            find_children_recursively(child, sample_index, child_transform, polymeshes);
+        }
+    }
 
     ScalarTransform4f read_abc_xform(const IObject& obj)
     {
@@ -604,5 +656,3 @@ private:
 MTS_IMPLEMENT_CLASS_VARIANT(AlembicMesh, Mesh)
 MTS_EXPORT_PLUGIN(AlembicMesh, "Alembic Mesh")
 NAMESPACE_END(mitsuba)
-
-
