011aafa646f3e864c522123dcc24fe64caeb25f6
Vincent Leroy <vincent.leroy@rayference.eu>
2020-09-21 15:42:29 +0200

    Added missing Python bindings for Volume API

--- src/librender/python/main_v.cpp
+++ src/librender/python/main_v.cpp
@@ -48,6 +48,7 @@ static py::object caster(Object *o) {
 
     PY_TRY_CAST(PhaseFunction);
     PY_TRY_CAST(Medium);
+    PY_TRY_CAST(Volume);
 
     return py::object();
 }
@@ -118,7 +119,7 @@ PYBIND11_MODULE(MODULE_NAME, m) {
     MTS_PY_IMPORT(ShapeKDTree);
     MTS_PY_IMPORT(srgb);
     MTS_PY_IMPORT(Texture);
-    // MTS_PY_IMPORT(Volume);
+    MTS_PY_IMPORT(Volume);
 
     /// Register the variant-specific caster with the 'core_ext' module
     auto casters = (std::vector<void *> *) (py::capsule)(
--- src/librender/python/texture_v.cpp
+++ src/librender/python/texture_v.cpp
@@ -33,3 +33,29 @@ MTS_PY_EXPORT(Texture) {
             vectorize(&Texture::pdf_position),
             "p"_a, "active"_a = true, D(Texture, pdf_position));
 }
+
+MTS_PY_EXPORT(Volume) {
+    MTS_PY_IMPORT_TYPES(Volume)
+    MTS_PY_CLASS(Volume, Object)
+        .def("eval",
+            vectorize(&Volume::eval),
+            "it"_a, "active"_a = true, D(Volume, eval))
+        .def("eval_1",
+            vectorize(&Volume::eval_1),
+            "it"_a, "active"_a = true, D(Volume, eval_1))
+        .def("eval_3",
+            vectorize(&Volume::eval_3),
+            "it"_a, "active"_a = true, D(Volume, eval_3))
+        .def("eval_gradient",
+            vectorize(&Volume::eval_gradient),
+            "it"_a, "active"_a = true, D(Volume, eval_gradient))
+        .def("max",
+            &Volume::max,
+            D(Volume, max))
+        .def("bbox",
+            &Volume::bbox,
+            D(Volume, bbox))
+        .def("resolution",
+            &Volume::resolution,
+            D(Volume, resolution));
+}
