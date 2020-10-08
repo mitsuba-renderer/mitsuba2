881df32618be4f7f9843913e6946940e848e8da3
Merlin Nimier-David <merlin.nimier@gmail.com>
2020-10-08 12:22:45 +0200

    ConstantEmitter: start implementing new interfaces

--- src/emitters/constant.cpp
+++ src/emitters/constant.cpp
@@ -39,6 +39,7 @@ public:
         /* Until `set_scene` is called, we have no information
            about the scene and default to the unit bounding sphere. */
         m_bsphere = ScalarBoundingSphere3f(ScalarPoint3f(0.f), 1.f);
+        m_inv_surface_area = ek::rcp(4.f * ek::Pi<ScalarFloat> * 1.f);
 
         m_radiance = props.texture<Texture>("radiance", Texture::D65(1.f));
         m_flags = +EmitterFlags::Infinite;
@@ -49,6 +50,8 @@ public:
         m_bsphere = scene->bbox().bounding_sphere();
         m_bsphere.radius = ek::max(math::RayEpsilon<Float>,
                                m_bsphere.radius * (1.f + math::RayEpsilon<Float>));
+        m_inv_surface_area =
+            ek::rcp(4.f * ek::Pi<ScalarFloat> * m_bsphere.radius * m_bsphere.radius);
     }
 
     Spectrum eval(const SurfaceInteraction3f &si, Mask active) const override {
@@ -106,12 +109,49 @@ public:
         };
     }
 
+    std::pair<PositionSample3f, Float>
+    sample_position(Float time, const Point2f &sample,
+                    Mask active) const override {
+        /* Note that sampling a position from an infinitely distant light source
+         * cannot really make sense. Instead, we return a position from a
+         * virtual bounding sphere around the scene. */
+        Vector3f d = warp::square_to_uniform_sphere(sample);
+
+        PositionSample3f ps;
+        ps.p      = m_bsphere.center + d * m_bsphere.radius;
+        ps.n      = -d;
+        ps.uv     = sample;
+        ps.time   = time;
+        ps.pdf    = ek::select(active, m_inv_surface_area, 0.f);
+        ps.delta  = false;
+        ps.object = this;
+        return { ps, ek::select(ps.pdf > 0.f, ek::rcp(m_inv_surface_area), 0.f) };
+    }
+
+    // std::pair<Spectrum, Spectrum>
+    // sample_wavelengths(Float sample, Mask active) const override {
+    //     // TODO: how to sample the spectrum without a position?
+    //     return m_radiance->sample_spectrum(si, math::sample_shifted<Wavelength>(sample), active);
+    // }
+
     Float pdf_direction(const Interaction3f &, const DirectionSample3f &ds,
                         Mask active) const override {
         MTS_MASKED_FUNCTION(ProfilerPhase::EndpointEvaluate, active);
 
         return warp::square_to_uniform_sphere_pdf(ds.d);
     }
+
+    Float pdf_position(const PositionSample3f & /*ps*/,
+                       Mask active) const override {
+        // Note that in principle, we should check that ps.p lies on the bsphere
+        return ek::select(active, m_inv_surface_area, 0.f);
+    }
+
+    // Spectrum pdf_wavelengths(const Spectrum &wavelengths,
+    //                          Mask active) const override {
+    //     // TODO: how to get pdf_spectrum without a position? Interface mismatch.
+    //     return m_radiance->pdf_spectrum(wavelengths, active);
+    // }
 
     /// This emitter does not occupy any particular region of space, return an invalid bounding box
     ScalarBoundingBox3f bbox() const override {
@@ -135,6 +175,9 @@ public:
 protected:
     ref<Texture> m_radiance;
     ScalarBoundingSphere3f m_bsphere;
+
+    /// Inverse surface area of the bounding sphere
+    ScalarFloat m_inv_surface_area;
 };
 
 MTS_IMPLEMENT_CLASS_VARIANT(ConstantBackgroundEmitter, Emitter)
