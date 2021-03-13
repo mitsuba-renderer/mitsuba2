7ad4f5588d41fe7ce45fdb35ef8d2a852844ea26
Merlin Nimier-David <merlin.nimier@gmail.com>
2021-03-13 17:56:14 +0100

    Scene::sample_emitter_direction: fix emitter selection weight

--- src/librender/scene.cpp
+++ src/librender/scene.cpp
@@ -263,13 +263,14 @@ Scene<Float, Spectrum>::sample_emitter_direction(const Interaction3f &ref, const
             // Randomly pick an emitter
             auto [index, emitter_weight, sample_re] = sample_emitter(sample.x(), active);
             sample.x() = sample_re;
-            // Account for the discrete probability of sampling this emitter
-            ds.pdf *= ek::rcp(emitter_weight);
-            spec *= emitter_weight;
 
             // Sample a direction towards the emitter
             EmitterPtr emitter = ek::gather<EmitterPtr>(m_emitters_ek, index, active);
             std::tie(ds, spec) = emitter->sample_direction(ref, sample, active);
+
+            // Account for the discrete probability of sampling this emitter
+            ds.pdf *= pdf_emitter(index, active);
+            spec *= emitter_weight;
         }
 
         active &= ek::neq(ds.pdf, 0.f);
